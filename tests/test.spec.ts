e1a3e9256b6450b29061797326d42b284563fb40
PablodlFR <alu0101336152@ull.edu.es>
2023-05-09 15:46:13 +0000

    Test GitHub Actions Coveralls

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,12 @@
 import * as request from 'supertest';
 import { app } from '../src/app';
+import { Track } from '../src/models/track';
 import { expect } from 'chai';
+
+beforeEach(async () => {
+  await Track.deleteMany();
+  //await new User(firstUser).save();
+});
 
 describe('POST /tracks', () => {
   it('Should successfully create a new track', async () => {
