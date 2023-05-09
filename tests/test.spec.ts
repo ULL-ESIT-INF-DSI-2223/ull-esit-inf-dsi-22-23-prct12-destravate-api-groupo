720b85cbc2f2e21c89d645a1cbe14ddfa1ca30c9
PablodlFR <alu0101336152@ull.edu.es>
2023-05-09 16:34:58 +0000

    Informe Update

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,27 +3,26 @@ import { app } from '../src/app';
 import { Track } from '../src/models/track';
 import { expect } from 'chai';
 
-// beforeEach(async () => {
-//   await Track.deleteMany();
-//   //await new User(firstUser).save();
-// });
+beforeEach(async () => {
+  await Track.deleteMany();
+});
 
 describe('POST /tracks', () => {
   it('Should successfully create a new track', async () => {
     await request(app).post('/tracks').send({
       startGeolocation: {
-        latitude: "1.54",
-        longitude: "2.32"
+        latitude: "28.3141 N",
+        longitude: "16.5538 W"
       },
       endGeolocation: {
-        latitude: "6.43",
-        longitude: "7.12"
+        latitude: "28.2186 N",
+        longitude: "16.7172 W"
       },
-      name: "Ruta87",
+      name: "Ruta de los volcanes",
       length: 12,
       unevenness: 20,
       activity: "bike",
-      rating: 6.8
+      rating: 7.2
     }).expect(201);
   });
 });
